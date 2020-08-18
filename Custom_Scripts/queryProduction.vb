4276059af425d55ceaab768d65eae693b00416d5
Estêvão J. dos Santos <esrevaojs@gmail.com>
2020-08-18 18:47:50 -0300

    Query with Search and Date Filter for Production

--- Custom_Scripts/queryProduction.vb
+++ Custom_Scripts/queryProduction.vb
@@ -1,14 +1,14 @@
 Function queryProduction(ByRef pConnection)
-'SELECIONA DADOS E ORDENA DE ACORDO COM PARAMETRO
+'Query ao DB manipulada pelo VBA do Tia Portal
 
-' SELECT prod.*, st.descricao_status_peca
-'  FROM [SNP_PI1846_7oEixo_Banco].[dbo].[tb_pr_producao] AS prod
-'  INNER Join [SNP_PI1846_7oEixo_Banco].[dbo].[tb_ana_status_peca] AS st On prod.Status_Peca = st.id_status_peca
-'  WHERE prod.Barcode LIKE '%filtro%'
-'	AND prod.Data BETWEEN 'dataINI' AND 'dataFIM'
-'  ORDER BY prod.ID DESC
+Dim rst, SQL_Table, strAscDesc, strFuncName, beginDate, endDate, search
 
-Dim rst, SQL_Table, strAscDesc, strFuncName
+'Essas Tags precisam ser criadas na IHM e associadas aos diplays de input
+beginDate = SmartTags("pFiltroDataInicial")
+endDate = SmartTags("pFiltroDataFinal")
+beginDate = Year(beginDate) & "-" & Month(beginDate) & "-" & Day(beginDate) & " 23:59"
+endDate = Year(endDate) & "-" & Month(endDate) & "-" & Day(endDate) & " 23:59"
+search = SmartTags("pSearchPN")
 
 
 On Error Resume Next
@@ -20,24 +20,20 @@ showLog "Entrei na query"
 SQL_Table = "USE hmiDB; " &_
 		"SELECT S.Producao_id, B.PNSerialString, M.ModeloString, M.NomeModelo, S.opBB155, S.opBB165, S.opBB175, S.opBB185, S.inspecao, B.dt_Entrada, S.dt_Saida " &_
 		"FROM RegEntradaBlocos AS B " &_
-    	"RIGHT JOIN RegSaidaBlocos AS S ON B.Bloco_id = S.Bloco_id " &_
-    	"INNER JOIN ModelosBlocos AS M ON B.Modelo_id = M.Modelo_id "
+    	"JOIN RegSaidaBlocos AS S ON B.Bloco_id = S.Bloco_id " &_
+    	"LEFT JOIN ModelosBlocos AS M ON B.Modelo_id = M.Modelo_id "
 
 'Filtro de datas
-'SQL_Table = SQL_Table & "WHERE prod.dt_Entrada BETWEEN '" & pFiltroDataInicial & "' AND '" & pFiltroDataFinal & "'  "
+SQL_Table = SQL_Table & "WHERE S.dt_Saida BETWEEN '" & beginDate & "' AND '" & endDate & "'  "
 
 			
 'Verifica se foi pedido filtro de PN
-'If pFiltroPN <> "" And Not SmartTags("CmdUpDown")  Then
-'	SQL_Table = SQL_Table & "AND prod.PNSerialString LIKE '%" & pFiltroPN & "%' "
-'End If	
-	
-'SmartTags("CmdUpDown")=False		
-			
-'Ordena
-'SQL_Table = SQL_Table & "ORDER BY prod." & pOrdem & " " & strAscDesc
+If search <> "" Then
+	SQL_Table = SQL_Table & "AND B.PNSerialString LIKE '%" & search & "%' "
+End If	
 
-SQL_Table = SQL_Table & " ;"
+'Ordena para padrão decrescente
+SQL_Table = SQL_Table & " ORDER BY S.dt_Saida DESC, S.Producao_id DESC;"
 
 'Se o Debug estiver ativado
 showLog "Select: " & SQL_Table
