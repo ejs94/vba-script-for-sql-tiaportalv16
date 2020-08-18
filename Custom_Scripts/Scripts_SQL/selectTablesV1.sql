a400cdb84b40eb30ad8da2dafd356000f4863206
Estêvão J. dos Santos <esrevaojs@gmail.com>
2020-08-18 17:48:24 -0300

    Corrigindo o bug de não aparecer valores NULL

--- Custom_Scripts/Scripts_SQL/selectTablesV1.sql
+++ Custom_Scripts/Scripts_SQL/selectTablesV1.sql
@@ -1,6 +1,7 @@
 --- Query to see the result:
 --- Select para cruzar as duas tabelas
+--- Atualizada para alguns bugs
 SELECT S.Producao_id, B.PNSerialString, M.ModeloString, M.NomeModelo, S.opBB155, S.opBB165, S.opBB175, S.opBB185, S.inspecao, B.dt_Entrada, S.dt_Saida
 FROM RegEntradaBlocos AS B
-    RIGHT JOIN RegSaidaBlocos AS S ON B.Bloco_id = S.Bloco_id
-    INNER JOIN ModelosBlocos AS M ON B.Modelo_id = M.Modelo_id;
+    JOIN RegSaidaBlocos AS S ON B.Bloco_id = S.Bloco_id
+    LEFT JOIN ModelosBlocos AS M ON B.Modelo_id = M.Modelo_id;
