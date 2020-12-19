71d9e51f1085590fb1e0fb2a2de7f413980ab90d
Estêvão J. dos Santos <estevaojs@gmail.com>
2020-12-19 03:12:21 -0300

    Update returnSQLString.vb

--- Jonfra_Scripts_VBA/Formguide/returnSQLString.vb
+++ Jonfra_Scripts_VBA/Formguide/returnSQLString.vb
@@ -5,7 +5,7 @@ Dim SQL_Seriais
 ''''''''''''''''''''''''''' STRINGS SQL ''''''''''''''''''''''''''''''''''''''''''''''''''''
 '''Query para buscar as Strings de todos os Turnos
 SQL_Seriais = " USE hmiDB; " &_
-                " SELECT S.dt_Saida,B.PNSerialString " &_
+                " SELECT S.dt_Saida, B.PNSerialString, S.opBB155, S.opBB165, S.opBB175, LTRIM(S.opBB185) " &_
                 " FROM RegEntradaBlocos AS B " &_
                 " JOIN RegSaidaBlocos AS S ON B.Bloco_id = S.Bloco_id " &_
                 " LEFT JOIN ModelosBlocos AS M ON B.Modelo_id = M.Modelo_id " &_
